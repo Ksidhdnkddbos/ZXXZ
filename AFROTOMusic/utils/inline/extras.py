ee91ee5ff4f61ea09e0f719d2dfe829cec80a3db
kyoot20230 <kyoot20230@gmail.com>
2024-05-24 23:55:18 +0300

    Update extras.py

--- AFROTOMusic/utils/inline/extras.py
+++ AFROTOMusic/utils/inline/extras.py
@@ -1,5 +1,6 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+from config import SUPPORT_CHAT
 
 
 def botplaylist_markup(_):
@@ -19,6 +20,20 @@ def close_markup(_):
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
+                ),
+            ]
+        ]
+    )
+    return upl
+
+
+def supp_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_9"],
+                    url=SUPPORT_CHAT,
                 ),
             ]
         ]
