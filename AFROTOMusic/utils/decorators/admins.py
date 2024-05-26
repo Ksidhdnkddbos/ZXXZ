229471bef1f3ecf5307af5d9cc360546a78236ac
kyoot20230 <kyoot20230@gmail.com>
2024-05-26 08:03:30 +0300

    Update admins.py
    hfj

--- AFROTOMusic/utils/decorators/admins.py
+++ AFROTOMusic/utils/decorators/admins.py
@@ -1,10 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AFROTOMusic import app
-from AFROTOMusic.misc import SUDOERS, db
-from AFROTOMusic.utils.database import (
-    get_authuser_names,
+from ADMANMusic import app
+from ADMANMusic.misc import SUDOERS, db
+from ADMANget_authuser_names,
     get_cmode,
     get_lang,
     get_upvote_count,
