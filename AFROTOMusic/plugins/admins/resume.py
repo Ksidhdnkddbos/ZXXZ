86ada9f49500532ffb126f7419a339f1e7fe6335
kyoot20230 <kyoot20230@gmail.com>
2024-05-26 08:19:13 +0300

    Update resume.py
    ghj

--- AFROTOMusic/plugins/admins/resume.py
+++ AFROTOMusic/plugins/admins/resume.py
@@ -1,16 +1,16 @@
-from AFROTOMusic.plugins.play.filters import command
+from ADMANMusic.plugins.play.filters import command
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AFROTOMusic import app
-from AFROTOMusic.core.call import Zelzaly
-from AFROTOMusic.utils.database import is_music_playing, music_on
-from AFROTOMusic.utils.decorators import AdminRightsCheck
-from AFROTOMusic.utils.inline import close_markup
+from ADMANMusic import app
+from ADMANMusic.core.call import Zelzaly
+from ADMANMusic.utils.database import is_music_playing, music_on
+from ADMANMusic.utils.decorators import AdminRightsCheck
+from ADMANMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(command(["استمر", "كملي"]) & ~BANNED_USERS)
+@app.on_message(command(["استمر", "كمل"]) & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
